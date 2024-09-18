7a4e33c94c93f354bb37464cf94356be8582be99
Badra Aliou SY <sybadraaliou@gmail.com>
2024-09-18 10:49:50 +0000

    Mise sur github

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'VolunteerDesk',
+  webDir: 'www'
+};
+
+export default config;
